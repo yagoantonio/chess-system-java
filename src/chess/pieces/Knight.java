7e8f7ba14a66f76e6f502a9738bafe6badf74fec
Yago Antonio <yago_antonio@live.com>
2019-08-15 13:31:19 -0300

    Queen

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -26,49 +26,41 @@ public class Knight extends ChessPiece {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0,0);
 		
-		// 
 		p.setValues(position.getRow()-1, position.getColumn()-2);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// 
 		p.setValues(position.getRow()-2, position.getColumn()-1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// 
 		p.setValues(position.getRow()-2, position.getColumn()+1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		// 
 		p.setValues(position.getRow()-1, position.getColumn()+2);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// 
 		p.setValues(position.getRow()+1, position.getColumn()+2);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		// 
 		p.setValues(position.getRow()+2, position.getColumn()+1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}		
 
-		// 
 		p.setValues(position.getRow()+2, position.getColumn()-1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		// 
 		p.setValues(position.getRow()+1, position.getColumn()-2);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
